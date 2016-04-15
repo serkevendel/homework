c34cb6e77019ec5963a657b4881cd3809a20f72a
Serke Vendel <serkevendel@gmail.com>
2016-04-15 23:54:11 +0200

    Added async homework and .gitignore

---
+++ homework-2-async/Async/Async-ejb/src/main/java/ejb/AsyncEJB.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.util.concurrent.Future;
+import javax.annotation.Resource;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Vendel
+ */
+@Stateless
+@Asynchronous
+public class AsyncEJB {
+    
+    @Resource
+    private SessionContext ctx;
+    
+    public Future<Integer> longTimeOperation(){
+        
+        int num=4000000;
+        int returnNum = 0;
+        
+        for(int i = 0; i<= num ; i++){
+            if(i == num)
+                returnNum=num;
+        }
+    
+    
+    if (ctx.wasCancelCalled()) {
+            return new AsyncResult<>(0);
+        } else {
+            return new AsyncResult<>(returnNum);
+        }
+    }
+}
---
+++ homework-2-async/Async/Async-web/src/main/java/REST/AsyncREST.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REST;
+
+import ejb.AsyncEJB;
+import java.io.Serializable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Vendel
+ */
+@Path("/async")
+@SessionScoped
+@Produces(MediaType.TEXT_PLAIN)
+public class AsyncREST implements Serializable{
+    
+    @Inject
+    private AsyncEJB asyncEJB;
+    
+    @GET
+    public String testAsync(@Context HttpServletRequest request) throws InterruptedException, ExecutionException{
+        HttpSession session = request.getSession(true);
+        session.setMaxInactiveInterval(1000);
+        Future<Integer> i1 = asyncEJB.longTimeOperation();
+        Future<Integer> i2 = asyncEJB.longTimeOperation();
+        i2.cancel(true);
+        Integer cancelledNum = i2.get();
+        Integer runned = i1.get();
+        return "Cancelled: "+ cancelledNum.toString() + "Runned: " + runned.toString();
+    }
+}
